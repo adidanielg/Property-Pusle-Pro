cfaad12e5284e1e6831b30ba1ba0b01f2c2e80e3
adidanielg <adida.gomez@gmail.com>
2026-03-13 17:44:15 -0400

    fix: fix enviroment

--- middleware/validateEnv.js
+++ middleware/validateEnv.js
@@ -2,13 +2,13 @@
 // Si falta una variable crítica, el servidor NO arranca
 
 const REQUIRED = [
-    'SUPABASE_URL',
-    'SUPABASE_KEY',
-    'SESSION_SECRET',
-    'ADMIN_USERNAME',
-    'ADMIN_PASSWORD',
-    'VAPID_PUBLIC_KEY',
-    'VAPID_PRIVATE_KEY',
+    'SUPABASE_URL = https://wqjzqjvhdhsglqgkzjnu.supabase.co',
+    'SUPABASE_KEY = sb_publishable_DZq6tx9bpppNevnk3advlg_vD18SBHW',
+    'SESSION_SECRE = w1ef1w98e1f98e1f9q8we441rf98q1df9q98wd1f99qw81ew98e1f9w8e1f',
+    'ADMIN_USERNAME = admin',
+    'ADMIN_PASSWORD = Deotor123*',
+    'VAPID_PUBLIC_KEY = BO6S5wAwnwxZqXJtfGBQniwzi2XKqkHvndoJodZrPzRMUUWV3Cc_YtIbmy3appADx55ldSAjW5lErYXABC_Fq5g', 
+    'VAPID_PRIVATE_KEY = XitAxzz5OBry3pWFhdYlini8CqVCn2vAMSRi1cGqmXQ',
 ];
 
 function validateEnv() {
